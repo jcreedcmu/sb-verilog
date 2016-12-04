ae46d723a15cf145c9de1b0fad0e5e0cf452cbc3
Jason Reed <jcreed@amazon.com>
2016-12-04 10:23:51 -0800

    can increment

--- hello.v
+++ hello.v
@@ -1,3 +1,13 @@
 module main;
-initial $interact;
+
+   initial $interact;
+
+   reg [3:0] val;
+
+   initial begin
+      val = 2;
+      $increment(val);
+      $display("After $increment, val=%d", val);
+   end
+
 endmodule
--- interact.c
+++ interact.c
@@ -9,6 +9,31 @@ static int interact_calltf(char *user_data) {
   return 0;
 }
 
+static int increment(char *userdata) {
+  vpiHandle systfref, args_iter, argh;
+  struct t_vpi_value argval;
+  int value;
+
+  // Obtain a handle to the argument list
+  systfref = vpi_handle(vpiSysTfCall, NULL);
+  args_iter = vpi_iterate(vpiArgument, systfref);
+
+  // Grab the value of the first argument
+  argh = vpi_scan(args_iter);
+  argval.format = vpiIntVal;
+  vpi_get_value(argh, &argval);
+  value = argval.value.integer;
+  vpi_printf("VPI routine received %d\n", value);
+
+  // Increment the value and put it back as first argument
+  argval.value.integer = value + 1;
+  vpi_put_value(argh, &argval, NULL, vpiNoDelay);
+
+  // Cleanup and return
+  vpi_free_object(args_iter);
+  return 0;
+}
+
 void interact_register() {
   s_vpi_systf_data tf_data;
 
@@ -21,7 +46,19 @@ void interact_register() {
   vpi_register_systf(&tf_data);
 }
 
+void register_increment() {
+  s_vpi_systf_data data;
+  data.type = vpiSysTask;
+  data.tfname = "$increment";
+  data.sizetf = 0;
+  data.user_data = 0;
+  data.calltf = increment;
+  data.compiletf = 0;
+  vpi_register_systf(&data);
+}
+
 void (*vlog_startup_routines[])() = {
   interact_register,
+  register_increment,
   0
 };
