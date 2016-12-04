3a0c608dddb9910e2d7021b324d0dacd1a266314
Jason Reed <jcreed@amazon.com>
2016-12-04 12:13:56 -0800

    synchronous input

--- hello.v
+++ hello.v
@@ -1,13 +1,23 @@
 module main;
+   parameter STDIN  = 32'h8000_0000;
 
-   initial $interact;
+//   initial $interact;
 
-   reg [3:0] val;
+   always
+     #1
+       if (!$feof(STDIN))
+         begin
+            $display("stdin %d", $fgetc(STDIN));
+         end
 
-   initial begin
-      val = 2;
-      $increment(val);
-      $display("After $increment, val=%d", val);
-   end
+   // initial begin
+   //    val = 2;
+   //    $increment(val);
+   //    $display("After $increment, val=%d", val);
+   // end
+
+
+initial
+  #20 $finish;     //finish after 20 time units
 
 endmodule
